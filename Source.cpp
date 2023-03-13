eaf6b670b5e16be2b48cdd974246beaa69b3e0fd
karanmec <67448875+karanmec@users.noreply.github.com>
2023-03-13 13:24:45 +0400

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+	float age=30.25;//int means number, num is variable
+	printf("the number is \n %f", age);//%d is placeholder of variable
+	//\n is to create a new line
+}
